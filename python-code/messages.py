a9327edca54b45562e426482a633ed871e44626b
japnitahuja <japnit.ahuja@gmail.com>
2023-04-07 18:27:52 -0400

    sending and receiving messages

---
+++ python-code/messages.py
@@ -0,0 +1,15 @@
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD  # accessing the communicator object
+rank = comm.rank  # getting the rank of the current process
+
+shared = rank+100
+
+if(rank == 0):
+    data = shared
+    comm.send(data, dest=1)  # sending data to process with rank of dest
+    print("From process", rank, "we sent", data)
+elif(rank == 1):
+    # receiving data from process with rank of source
+    data = comm.recv(source=0)
+    print("On process", rank, "we received", data)
