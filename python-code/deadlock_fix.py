560812ab30c1f11d65d87a10a2a11fc6fe82045d
japnitahuja <japnit.ahuja@gmail.com>
2023-04-12 21:47:59 -0400

    bug fix

--- python-code/deadlock_fix.py
+++ python-code/deadlock_fix.py
@@ -5,12 +5,12 @@ rank = comm.Get_rank()
 
 randNum = numpy.zeros(1)
 
-if rank == 1:
+if rank == 0:
     print("Process", rank, "before receiving has the number", randNum[0])
     comm.Irecv(randNum, source=1)
     print("Process", rank, "received the number", randNum[0])
 
-if rank == 0:
+if rank == 1:
     print("Process", rank, "before receiving has the number", randNum[0])
-    comm.Irecv(randNum, source=1)
+    comm.Irecv(randNum, source=0)
     print("Process", rank, "received the number", randNum[0])
