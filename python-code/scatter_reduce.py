70ee57c040e2599a16b084fdc0ef6e539c507691
japnitahuja <japnit.ahuja@gmail.com>
2023-04-12 23:26:27 -0400

    output

--- python-code/scatter_reduce.py
+++ python-code/scatter_reduce.py
@@ -6,16 +6,16 @@ rank = comm.rank
 size = comm.size
 
 if rank == 0:
-    data = np.arange(1, size + 1)
+    data = np.arange(1, size + 1, dtype=int)
 else:
-    data = None
+    data = np.zeros(size, dtype=int)
 
-# Scatter the data array to all processes
-data = comm.bcast(data, root=0)
+# broadcast the data array to all processes
+data = comm.scatter(data, root=0)
 print("Process", rank, "has received ", data)
 
 # Reduce the local sum to obtain the global sum
-global_sum = comm.reduce(data, op=MPI.SUM, root=0)
+global_sum = comm.reduce(data, MPI.SUM, 0)
 
 # Print the result on the root process
 if rank == 0:
